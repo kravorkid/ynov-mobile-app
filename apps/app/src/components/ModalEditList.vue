6b187e09bf04b49304f8069ef4bab4bc3af3e136
matthieu <matthieu.tabarin@gmail.com>
2023-03-03 09:52:32 +0100

    ✨ Modal edit list

---
+++ apps/app/src/components/ModalEditList.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-dialog v-model="attrs">
+    <q-card>
+      <q-card-section>
+        <p>Modal d'edition</p>
+        <q-input v-model="title" label="title" outlined />
+        {{ list }}
+        <q-btn @click="handleSave" label="update list"/>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+<script setup>
+import { ref, useAttrs } from 'vue'
+
+const title = ref('')
+const attrs = useAttrs()
+
+defineProps({
+  list: {
+    type: Object,
+    default: () => ({}),
+    required: true
+  }
+})
+
+const handleSave = async () => {
+  console.log('Save some data')
+}
+</script>
